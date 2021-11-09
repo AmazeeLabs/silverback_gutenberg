f1b9274eae96ca01f41eac05682bbad9b766802c
K.I.T.T <kitt@amazee.com>
2021-11-09 11:37:05 +0000

    chore: release version 1.3.2

--- silverback_gutenberg.api.php
+++ silverback_gutenberg.api.php
@@ -1,16 +1,35 @@
 <?php
 
+/**
+ * @deprecated Use hook_silverback_gutenberg_link_processor_block_attrs_alter
+ */
 function hook_silverback_gutenberg_link_processor_block_attributes_alter(
   array &$attributes,
   string $blockName,
   callable $processUrlCallback
 ) {
-  if ($blockName === 'custom/my-block' && isset($attributes['urls'])) {
+  // Use hook_silverback_gutenberg_link_processor_block_attrs_alter() instead.
+}
+
+/**
+ * @param array $attributes
+ * @param array $context
+ *   Has the following keys:
+ *   - blockName: a string containing the block name
+ *   - processUrlCallback: a callback to process a single URL string
+ *   - processLinksCallback: a callback to process links in an HTML string
+ */
+function hook_silverback_gutenberg_link_processor_block_attrs_alter(array &$attributes, array $context) {
+  if ($context['blockName'] === 'custom/my-links-block' && isset($attributes['urls'])) {
     $attributes['urlsUnprocessed'] = [];
     foreach ($attributes['urls'] as $key => $url) {
       $attributes['urlsUnprocessed'][$key] = $url;
-      $attributes['urls'][$key] = $processUrlCallback($url);
+      $attributes['urls'][$key] = $context['processUrlCallback']($url);
     }
+  }
+  if ($context['blockName'] === 'custom/my-media-block' && isset($attributes['caption'])) {
+    $html = $attributes['caption'];
+    $attributes['caption'] = $context['processLinksCallback']($html);
   }
 }
 
@@ -27,7 +46,7 @@ function hook_silverback_gutenberg_link_processor_outbound_link_alter(
   \Drupal\silverback_gutenberg\LinkProcessor $linkProcessor
 ) {
   $url = $link->getAttribute('href');
-  if ($url && !$linkProcessor->linksToCurrentHost()) {
+  if ($url && !$linkProcessor->linksToCurrentHost($url)) {
     $link->setAttribute('target', '_blank');
     $link->setAttribute('rel', 'noreferrer');
   }
